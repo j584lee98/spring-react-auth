5e632f8d24b307da827bc8cff20832cea1e67c81
j584lee98 <57649385+j584lee98@users.noreply.github.com>
2020-09-06 18:30:41 -0400

    Initial commit

---
+++ src/main/java/com/james/springreactauth/SpringReactAuthApplication.java
@@ -0,0 +1,13 @@
+package com.james.springreactauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/james/springreactauth/SpringReactAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.james.springreactauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
