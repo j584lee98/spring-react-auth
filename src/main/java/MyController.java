ce04ee9b51c74abce9acb50e844b9180b87fae98
j584lee98 <57649385+j584lee98@users.noreply.github.com>
2020-09-08 11:13:18 -0400

    Add controller for routes

---
+++ src/main/java/MyController.java
@@ -0,0 +1,30 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.james.springreactauth.entity.User;
+import com.james.springreactauth.repository.UserRepository;
+
+@Controller
+public class MyController {
+
+	@Autowired
+	UserRepository repo;
+
+	@RequestMapping(value = { "/", "/login", "/register" })
+	public String index() {
+		return "index.html";
+	}
+
+	@PostMapping("/register")
+	public String save(@ModelAttribute User user) {
+		try {
+			repo.save(user);
+		} catch (Exception e) {
+			return "redirect:/register?error";
+		}
+		return "redirect:/";
+	}
+}
