6a6ec86210074f9172960dc820591ae73eff6cd0
j584lee98 <57649385+j584lee98@users.noreply.github.com>
2020-09-08 23:45:17 -0400

    Add register html form

--- src/main/frontend/src/views/register/Register.js
+++ src/main/frontend/src/views/register/Register.js
@@ -1,5 +1,19 @@
 import React from "react";
 
-export default ()=>{
-  return <div>Register</div>
-}
+export default () => {
+  return (
+    <div>
+      <form id="register-form" action="/register" method="POST">
+        <div>
+          <div>Username:</div>
+          <input id="username" type="text" name="userName" />
+        </div>
+        <div>
+          <div>Password:</div>
+          <input id="password" type="password" name="password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
