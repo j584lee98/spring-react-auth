23580359084882aa3e862d4f7d3c3cbe87714ccd
j584lee98 <57649385+j584lee98@users.noreply.github.com>
2020-09-08 23:40:39 -0400

    Add login html form

--- src/main/frontend/src/views/login/Login.js
+++ src/main/frontend/src/views/login/Login.js
@@ -1,5 +1,19 @@
 import React from "react";
 
-export default ()=>{
-  return <div>Login</div>
-}
+export default () => {
+  return (
+    <div>
+      <form id="login-form" action="/login" method="POST">
+        <div>
+          <div>Username:</div>
+          <input id="username" type="text" />
+        </div>
+        <div>
+          <div>Password:</div>
+          <input id="password" type="password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
